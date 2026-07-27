0ef83e50c378850811a6d8ea26095cf564f492b1
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-27 17:10:40 +0530

    Update chatsdb.py

--- Chizuru/core/mongo/chatsdb.py
+++ Chizuru/core/mongo/chatsdb.py
@@ -5,35 +5,22 @@ mongo = MongoCli(MONGO_URL)
 db = mongo.chats
 db = db.chatsdb
 
-
 async def get_chats():
-  chat_list = []
-  async for chat in db.chats.find({"chat": {"$lt": 0}}):
-    chat_list.append(chat['chat'])
-  return chat_list
+    chat_list = []
+    async for chat in db.chats.find({"chat": {"$lt": 0}}):
+        chat_list.append(chat['chat'])
+    return chat_list
 
 async def get_chat(chat):
-  chats = await get_chats()
-  if chat in chats:
-    return True
-  else:
-    return False
+    chats = await get_chats()
+    return chat in chats
 
 async def add_chat(chat):
-  chats = await get_chats()
-  if chat in chats:
-    return
-  else:
-    await db.chats.insert_one({"chat": chat})
+    chats = await get_chats()
+    if chat not in chats:
+        await db.chats.insert_one({"chat": chat})
 
 async def del_chat(chat):
-  chats = await get_chats()
-  if not chat in chats:
-    return
-  else:
-    await db.chats.delete_one({"chat": chat})
-
-
-
-
-
+    chats = await get_chats()
+    if chat in chats:
+        await db.chats.delete_one({"chat": chat})
