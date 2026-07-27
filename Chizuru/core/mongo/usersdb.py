586b88bd7be5614507656ac1a0786f2f0b6eb431
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-27 17:09:43 +0530

    Update usersdb.py

--- Chizuru/core/mongo/usersdb.py
+++ Chizuru/core/mongo/usersdb.py
@@ -5,35 +5,22 @@ mongo = MongoCli(MONGO_URL)
 db = mongo.users
 db = db.usersdb
 
-
-
-
 async def get_users():
-  user_list = []
-  async for user in db.users.find({"user": {"$gt": 0}}):
-    user_list.append(user['user'])
-  return user_list
-
+    user_list = []
+    async for user in db.users.find({"user": {"$gt": 0}}):
+        user_list.append(user['user'])
+    return user_list
 
 async def get_user(user):
-  users = await get_users()
-  if user in users:
-    return True
-  else:
-    return False
+    users = await get_users()
+    return user in users
 
 async def add_user(user):
-  users = await get_users()
-  if user in users:
-    return
-  else:
-    await db.users.insert_one({"user": user})
-
+    users = await get_users()
+    if user not in users:
+        await db.users.insert_one({"user": user})
 
 async def del_user(user):
-  users = await get_users()
-  if not user in users:
-    return
-  else:
-    await db.users.delete_one({"user": user})
-
+    users = await get_users()
+    if user in users:
+        await db.users.delete_one({"user": user})
