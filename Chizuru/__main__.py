6ddd656792184d5af5d61a99b039c6b87d2f4c7c
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-07-27 17:04:08 +0530

    Update __main__.py

--- Chizuru/__main__.py
+++ Chizuru/__main__.py
@@ -3,10 +3,7 @@ import importlib
 from pyrogram import idle
 from Chizuru.modules import ALL_MODULES
 
- 
-
 loop = asyncio.get_event_loop()
-
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
@@ -14,7 +11,6 @@ async def sumit_boot():
     print("»»»» ʜᴇʀᴏᴋᴏ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
     await idle()
     print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
-
 
 if __name__ == "__main__":
     loop.run_until_complete(sumit_boot())
